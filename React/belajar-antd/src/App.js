ea65ff622b3f9b4f9f5ce6b3b61f827ec9c7dfb3
chamida12 <khamidaumami@gmail.com>
2022-06-14 09:16:48 +0700

    proyek

---
+++ React/belajar-antd/src/App.js
@@ -0,0 +1,12 @@
+import Dasboard from './Component/Dasboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Dasboard/>
+    </div>
+  );
+}
+
+export default App;
