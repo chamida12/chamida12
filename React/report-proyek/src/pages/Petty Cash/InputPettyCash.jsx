4fbbc3c607ba2065861654a50af6d0bbfb527c65
chamida12 <khamidaumami@gmail.com>
2022-07-02 19:44:26 +0700

    input Petty Cash

--- React/report-proyek/src/pages/Petty Cash/InputPettyCash.jsx
+++ React/report-proyek/src/pages/Petty Cash/InputPettyCash.jsx
@@ -1,22 +1,40 @@
-import { Card } from "antd";
+import { Card, Form, Input } from "antd";
 import React from "react";
 import './InputPettyCash.css'
 import { useNavigate } from "react-router-dom";
+import FormItem from "antd/lib/form/FormItem";
 
 export const InputPettyCash = () => {
     const navigate = useNavigate ()
+    
 
 return(
     <div>
-        <Card style={{margin:"auto",width:"1000px"}}>
-            <div>
-            <p>yui</p>
-            <p>yui</p>
-            <p>yui</p>
+        <div style={{padding:"50px"}}>
+            <p style={{textAlign:"center",fontWeight:"bold",fontSize:15}}> PETTY CASH </p>
+             <Card style={{margin:"auto",width:"1000px"}}>
+                 <div>
+                     <Form style={{margin:"50px"}}>
+                         <FormItem style={{fontWeight:"bold", margin:"30px",textAlign:"center"}} label="No Akun  ">
+                             <Input style={{width:"600px"}}/>
+                         </FormItem>
+                         <FormItem style={{fontWeight:"bold", margin:"30px",textAlign:"center"}} label="Nominal">
+                             <div>
 
-            </div>
+                             <Input style={{width:"600px"}}/>
+                             </div>
+                         </FormItem>
+                         <FormItem style={{fontWeight:"bold", margin:"30px",textAlign:"center"}} label="Debit">
+                             <Input style={{width:"600px"}}/>
+                         </FormItem>
+                         <FormItem style={{fontWeight:"bold", margin:"30px",textAlign:"center"}} label="Keterangan">
+                             <Input style={{width:"600px"}}/>
+                         </FormItem>
+                     </Form>
+                 </div>
 
-        </Card>
+             </Card>
+        </div>
    
     </div>
 )
